def318f1d82946b36d8a79dcbe5ece61e5e86854
Samed Kütahyalı <smdkthyli@gmail.com>
2024-07-14 09:16:24 +0300

    add masstransit configuration for all services

--- SagaStateMachine.Service/Program.cs
+++ SagaStateMachine.Service/Program.cs
@@ -1,6 +1,14 @@
-using SagaStateMachine.Service;
+using MassTransit;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingRabbitMq((context, configure) =>
+    {
+        configure.Host(builder.Configuration["RabbitMQ"]);
+    });
+});
 
 var host = builder.Build();
 host.Run();
