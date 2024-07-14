59f6809b2c6ac9b07268e291261216493ab22e3f
Samed Kütahyalı <smdkthyli@gmail.com>
2024-07-14 10:28:02 +0300

    add order state instance, order state map, order state db context and order state machine

--- SagaStateMachine.Service/Program.cs
+++ SagaStateMachine.Service/Program.cs
@@ -1,9 +1,22 @@
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using SagaStateMachine.Service.StateDbContexts;
+using SagaStateMachine.Service.StateInstances;
+using SagaStateMachine.Service.StateMachines;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddMassTransit(configurator =>
 {
+    configurator.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>()
+        .EntityFrameworkRepository(options =>
+        {
+            options.AddDbContext<DbContext, OrderStateDbContext>((provider, _builder) =>
+            {
+                _builder.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer"));
+            });
+        });
+
     configurator.UsingRabbitMq((context, configure) =>
     {
         configure.Host(builder.Configuration["RabbitMQ"]);
---
+++ SagaStateMachine.Service/StateMachines/OrderStateMachine.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+using SagaStateMachine.Service.StateInstances;
+
+namespace SagaStateMachine.Service.StateMachines
+{
+    public class OrderStateMachine : MassTransitStateMachine<OrderStateInstance>
+    {
+        public OrderStateMachine()
+        {
+            InstanceState(instance => instance.CurrentState);
+        }
+    }
+}
