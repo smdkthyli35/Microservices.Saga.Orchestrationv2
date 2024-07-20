61d581aab83583117928e6284be1b6a1b0031e73
Samed Kütahyalı <smdkthyli@gmail.com>
2024-07-20 09:33:18 +0300

    add order service consumers

---
+++ Order.API/Consumers/OrderCompletedEventConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Order.API.Contexts;
+using Order.API.Enums;
+using Shared.OrderEvents;
+
+namespace Order.API.Consumers
+{
+    public class OrderCompletedEventConsumer(OrderDbContext orderDbContext) : IConsumer<OrderCompletedEvent>
+    {
+        public async Task Consume(ConsumeContext<OrderCompletedEvent> context)
+        {
+            Models.Order? order = await orderDbContext.Orders.FindAsync(context.Message.OrderId);
+            if (order is not null)
+            {
+                order.OrderStatus = OrderStatus.Completed;
+                await orderDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
