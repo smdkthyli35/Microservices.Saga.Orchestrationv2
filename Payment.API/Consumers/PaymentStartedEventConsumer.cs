29c37dd77e457517903f508f5948ce069c050dc5
Samed Kütahyalı <smdkthyli@gmail.com>
2024-07-20 10:04:34 +0300

    add PaymentStartedEventConsumer

---
+++ Payment.API/Consumers/PaymentStartedEventConsumer.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using Shared.PaymentEvents;
+using Shared.Settings;
+
+namespace Payment.API.Consumers
+{
+    public class PaymentStartedEventConsumer(ISendEndpointProvider sendEndpointProvider) : IConsumer<PaymentStartedEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentStartedEvent> context)
+        {
+            var sendEndpoint = await sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettings.StateMachineQueue}"));
+
+            if (true)
+            {
+                PaymentCompletedEvent paymentCompletedEvent = new(context.Message.CorrelationId)
+                {
+                };
+
+                await sendEndpoint.Send<PaymentCompletedEvent>(paymentCompletedEvent);
+            }
+            else
+            {
+                PaymentFailedEvent paymentFailedEvent = new(context.Message.CorrelationId)
+                {
+                    OrderItems = context.Message.OrderItems,
+                    Message = "Yetersiz bakiye!"
+                };
+
+                await sendEndpoint.Send<PaymentFailedEvent>(paymentFailedEvent);
+            }
+        }
+    }
+}
